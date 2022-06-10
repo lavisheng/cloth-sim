2f8c9c87607487c8ef21a71fd8c7ec1c83c00661
lavisheng <hwangcodes@gmail.com>
2022-06-09 23:31:29 -0400

    Changed the function inputs and added some documentation

--- include/stretch.h
+++ include/stretch.h
@@ -1,7 +1,28 @@
 #include <Eigen/Dense>
+/**
+ * Calculates C(x) for the stretch condition
+ * Inputs:
+ *   wuv - wuv matrix
+ *   b - scalars controlling desired stretch amount in uv respectively
+ *   a - area of triangle in (u,v) space (precomputable
+ */
+void stretch_cond(Eigen::Vector2d &cond, Eigen::MatrixXd wuv, Eigen::Vector2d b, double a, Eigen::RowVector3d coords);
 
-void stretch_cond(Eigen::Vector2d &cond, Eigen::MatrixXd wuv, Eigen::Vector2d b, Eigen::Vector2d uv, Eigen::RowVector3d coords);
-
+/**
+ * Precomputes matrix values that are used to calculate dcdxi
+ * Inputs:
+ *   F - #F x 3 matrix storing indices to vertices
+ *   UV - #UV by 2 list of UV coordinates
+ */
 void stretch_precompute(Eigen::MatrixXd &stretch_dwudx, Eigen::MatrixXd &stretch_dwvdx, Eigen::MatrixXi F, Eigen::MatrixXd UV);
 
+/**
+ * Computes dcdxi for a triangle
+ * Inputs:
+ *   i0 - triangle id
+ *   i1 - vertex in triangle id
+ *   wuv - wuv matrix
+ *   stretch_dwudx - precomputed dwudx matrix
+ *   stretch_dwvdx - precomputed dwvdx
+ */
 void stretch_dcdxi(Eigen::MatrixXd & dcdxi, int i0, int i1, Eigen::MatrixXd wuv, Eigen::MatrixXd stretch_dwudx, Eigen::MatrixXd stretch_dwvdx);
--- src/stretch.cpp
+++ src/stretch.cpp
@@ -1,7 +1,7 @@
 #include "stretch.h"
 #include "w_matrix.h"
 
-void stretch_cond(Eigen::Vector2d &cond, Eigen::MatrixXd wuv, Eigen::Vector2d b, double a, Eigen::Vector2d uv, Eigen::RowVector3d coords){
+void stretch_cond(Eigen::Vector2d &cond, Eigen::MatrixXd wuv, Eigen::Vector2d b, double a){
   cond(0) = a * (wuv.col(0).norm() - b(0));
   cond(1) = a * (wuv.col(1).norm() - b(1));
 }
@@ -13,9 +13,11 @@ void stretch_precompute(Eigen::MatrixXd &stretch_dwudx, Eigen::MatrixXd &stretch
   Eigen::MatrixXd duv(2,2);
   double D;
   for(int i = 0; i < F.rows(); i++){
+    // basically use what we did in w_matrix to get duv
     tri = F.row(i);
     duv << UV(i * 3 + tri(1), 0) - UV(i * 3 + tri(0), 0), UV(i * 3 + tri(2), 0) - UV(i * 3 + tri(0), 0),
       UV(i * 3 + tri(1),1) - UV(i * 3 + tri(1), 1), UV(i * 3 + tri(2), 1) - UV(i * 3 + tri(0), 1);
+    // the denominator du1 * dv2 - du2 * dv1
     D = duv(0,0) * duv(1,1) - duv(0,1) * duv(1,0);
     stretch_dwudx.row(i) << (duv(1,0) - duv(1,1))/ D, duv(1,0)/ D, duv(1,1) / D;
     stretch_dwvdx.row(i) << (duv(0,0) - duv(0,1))/ D , duv(0,0)/ D, duv(0,1) / D;
@@ -23,9 +25,7 @@ void stretch_precompute(Eigen::MatrixXd &stretch_dwudx, Eigen::MatrixXd &stretch
 }
 void stretch_dcdxi(Eigen::MatrixXd &dcdxi, int i0, int i1, Eigen::MatrixXd wuv, Eigen::MatrixXd stretch_dwudx, Eigen::MatrixXd stretch_dwvdx){
   dcdxi.resize(3,2);
-  // we can precompute the matrices we need to calculate, and then choose and store them
-  // in ucomp, vcomp
-  // TODO: do the precomputation steps.
+  // we use precomputed to form the appropriate matrices
   Eigen::Matrix3d ucomp, vcomp;
   ucomp = stretch_dwudx(i0, i1) * Eigen::Matrix3d::Identity();
   vcomp = stretch_dwvdx(i0,i1) * Eigen::Matrix3d::Identity();
