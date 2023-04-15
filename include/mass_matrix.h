85a76a6f08a81b2f7076028b65db8a23ca96d4d7
Rikin Gurditta <dolllyamar@gmail.com>
2023-04-15 18:53:28 -0400

    Implemented mass matrix and copied linearly implicit euler from 417 assignment

--- include/mass_matrix.h
+++ include/mass_matrix.h
@@ -1,4 +1,4 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 
-void mass_matrix(Eigen::SparseMatrix<double> &M);
+void mass_matrix(Eigen::SparseMatrix<double> &M, Eigen::MatrixXd &V, double m0);
--- src/mass_matrix.cpp
+++ src/mass_matrix.cpp
@@ -1,4 +1,7 @@
 #include "mass_matrix.h"
-void mass_matrix(Eigen::SparseMatrix<double> &M){
 
+void mass_matrix(Eigen::SparseMatrix<double> &M, Eigen::MatrixXd &V, double m0){
+  M = Eigen::SparseMatrix<double>(V.rows() * 3, V.rows() * 3);
+  M.setIdentity();
+  M *= m0;
 }
